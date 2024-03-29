c0d93a62e0781739897d66a833483fe48246c77a
Kerwin_Wang <tomatowk@aliyun.com>
2024-03-29 11:52:24 +0800

    Create AccountInfo.cs

---
+++ Unity/src/AccountInfo.cs
@@ -0,0 +1,8 @@
+
+public class AccountInfo
+{
+    //AppID
+    public static string TEST_AppID = "";   //请从官网注册获取AppID、AppScret
+    //AppScret
+    public static string TEST_AppScret = "";    //请从官网注册获取AppID、AppScret
+}

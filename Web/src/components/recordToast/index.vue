a213841f5f285655a91ffab687a2638097eaba88
wiesen <wwdn7@vip.qq.com>
2025-12-01 11:35:10 +0800

    web update 1.3.2

--- Web/src/components/recordToast/index.vue
+++ Web/src/components/recordToast/index.vue
@@ -23,6 +23,8 @@
 import { jsonp } from '@/utils'
 import { getToken } from '@/utils/auth'
 import Cookies from 'js-cookie'
+import MD5 from 'crypto-js/md5'
+
 export default {
   filters: {
     // 四舍五入保留两位小数
@@ -58,14 +60,38 @@ export default {
     // 调用服务端api，获取录像下载地址
     getDownload() {
       const { AppId, MD5_AppSecret } = JSON.parse(getToken())
+      const serverAddr = Cookies.get('addr');
       const data = {
         RequestId: '' + new Date().getTime(),
-        UserName: AppId,
-        UserPswd: MD5_AppSecret,
         fileName: this.info.fileName
       }
+
+      // appID为‘默认’的情况下，要用compID和compSecret鉴权
+      if (AppId === '默认') {
+        // 自建环境CompID和CompSecret默认都是1
+        let CompID = 1;
+        let CompSecret = 1;
+        if (serverAddr.includes('crlab.cloudr')) {
+          // 内网25环境
+          CompID = 213213;
+          CompSecret = '7859f2ee1064f3fac228b1792f8ca48b';
+        } else if (serverAddr.includes('sdk.cloudr')) {
+          // 公有云环境
+          CompID = 213213;
+          CompSecret = '1hm4fn0lop79oyz7kjorzp0szis95uia';
+        }
+        data['CompID'] = CompID;
+        data['SecretKey'] = MD5((`${CompID}&${CompSecret}`).toString());
+
+        // appID不为‘默认’的情况下，可以用userName和userPswd鉴权
+      } else {
+        data['UserName'] = AppId;
+        data['UserPswd'] = MD5_AppSecret;
+      }
+
+
       jsonp({
-        url: `https://${Cookies.get('addr')}/CLOUDMEETING-API/netDisk/query`,
+        url: `https://${serverAddr}/CLOUDMEETING-API/netDisk/query`,
         data,
         success: (data) => {
           try {
